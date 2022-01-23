617edbb152545710b29aa72c300504ee61d689fa
Daniel Wiklund <daniel@happyrabbit.com>
2022-01-23 23:06:30 +0800

    Disable PriceListComponent from product schema

--- schemas/documents/product/product.js
+++ schemas/documents/product/product.js
@@ -195,7 +195,7 @@ export default {
       of: [
         { type: 'scoped.productPrice' },
       ],
-      inputComponent: PriceListComponent,
+      //inputComponent: PriceListComponent,
       fieldset: 'eCommerce',
     },
     {
